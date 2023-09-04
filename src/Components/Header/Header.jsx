809ccefd7e2f8015a2e8dc97050c3e7dff2590a7
Egor Ivankov <egor.ivankov2015@bk.ru>
2023-09-04 18:10:08 +0300

    added scrinshots

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './header.scss';
-import moon from '../../img/moon.png';
-import sun from '../../img/sun.png'
 const Header = () => {
-    const [light, setLight] = useState(true);
-
-    const onSelectLight = () => {
-        setLight(!light);
-    }
 
     return (
         <div className='header'>
             <h1>Currency converter</h1>
-            <img className="light-icon" onClick={onSelectLight} src={light ? sun : moon} alt="light-icon" />
         </div>
     );
 }
